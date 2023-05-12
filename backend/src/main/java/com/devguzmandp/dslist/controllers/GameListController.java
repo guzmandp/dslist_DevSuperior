6717b3e8152e6a2691083fd9270780b2841fd094
guzmandp <guzmandp@gmail.com>
2023-05-11 23:03:37 -0300

    Move games on list

--- backend/src/main/java/com/devguzmandp/dslist/controllers/GameListController.java
+++ backend/src/main/java/com/devguzmandp/dslist/controllers/GameListController.java
@@ -1,15 +1,19 @@
 package com.devguzmandp.dslist.controllers;
 
+import java.awt.PageAttributes.MediaType;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devguzmandp.dslist.dto.GameListDTO;
 import com.devguzmandp.dslist.dto.GameMinDTO;
+import com.devguzmandp.dslist.dto.ReplacementDTO;
 import com.devguzmandp.dslist.services.GameListService;
 import com.devguzmandp.dslist.services.GameService;
 
@@ -35,4 +39,8 @@ public class GameListController {
 		return result;
 		}
 	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+		}
 }
